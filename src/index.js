ef29f053e3c4a27d0e341d222651b9b2b678439f
Kate-13 <kate_boyko@mail.ru>
2020-10-03 00:49:43 +0300

    feat: implement max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        let minel = array[0];
+        array.forEach((element) => {
+            if(element < minel){
+                minel = element;
+            }
+        });
+        return minel;
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        let maxel = array[0];
+        array.forEach((element) => {
+            if(element > maxel){
+                maxel = element;
+            }
+        });
+        return maxel;
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array && array.length !== 0) {
+        let avgvalue = array[0];
+        let sum = 0;
+        let count = 0;
+        array.forEach((element) => {
+            sum = sum + element;
+            count = count+1;
+        });
+        avgvalue = sum/count;
+        return avgvalue;
+    } else {
+        return 0;
+    }
 }
